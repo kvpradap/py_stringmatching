d6762da96472455c84ec9f551143ea27148f7ac7
kvpradap <pradap@cs.wisc.edu>
2016-01-29 12:35:08 -0600

    First commit with initial implementations

---
+++ setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup
+
+# Set this to True to enable building extensions using Cython.
+# Set it to False to build extensions from the C file (that
+# was previously created using Cython).
+# Set it to 'auto' to build with Cython if available, otherwise
+# from the C file.
+
+
+
+import sys
+
+
+
+setup(
+        name='py-stringmatching',
+        version='0.1',
+        description='Python library for string matching.',
+        long_description="""
+    String matching is an important problem in many settings such as data integration, natural language processing,etc.
+    This package aims to implement most commonly used string matching measures.
+    """,
+        url='http://github.com/kvpradap/py-stringmatching',
+        author='Pradap Konda',
+        author_email='pradap@cs.wisc.edu',
+        license=['MIT'],
+        packages=['py-stringmatching'],
+        install_requires=[
+            'numpy >= 1.7.0',
+            'six',
+            'python-Levenshtein >= 0.12.0',
+            'Cython >= 0.23.0',
+            'PyPrind >= 2.9.3'
+        ],
+        include_package_data=True,
+        zip_safe=False
+)
